5058ea58b0072313c9b946ea37869ac407590269
LopessSZzz <paulo.lopes2703@gmail.com>
2023-09-18 19:46:03 -0300

    Login

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="https://svelte.dev/">Svelte</a>
+      <a class="navbar-brand" href="/">Svelte</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -10,9 +10,9 @@
             <a class="nav-link active" aria-current="page" href="https://getbootstrap.com/">Bootstrap</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/Catalog">Catálogo</a>
+            <a class="nav-link" href="/catalog">Catálogo</a>
           </li>
-          <li class="nav-item dropdown">
+          <li class="nav-item dropdown active">
             <a class="nav-link dropdown-toggle" href="https://www.alura.com.br/artigos/svelte-versus-react-quais-diferencas" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Sobre este projeto
             </a>
@@ -21,6 +21,9 @@
               <li><a class="dropdown-item" href="https://www.youtube.com/">Another action</a></li>
               <li><a class="dropdown-item" href="https://www.youtube.com/">Something else here</a></li>
             </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login"><strong>Login</strong></a>
           </li>
         </ul>
       </div>
